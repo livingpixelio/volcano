93fe77fe0d008fecc69a2f5ec980c61e6f80d206
Casey Ydenberg <hi@caydenberg.io>
2026-02-04 09:25:39 -0800

    Export isLeaf for easier Mdast drafting

--- src/mod.ts
+++ src/mod.ts
@@ -5,3 +5,5 @@ export { openVault } from "./vault.ts";
 export { createCacheAdapterMemory } from "./adapters/CacheAdaptorMemory.ts";
 
 export { Transformers } from "./transform/index.ts";
+
+export { isLeaf } from "./parsers/markdown/MdastNode.ts";
